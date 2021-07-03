414669798ce1e8c58456cb1959a59211dfad821f
Akshay Pandey <codeakshay31@gmail.com>
2021-07-04 00:42:33 +0530

    added authRoutes

---
+++ src/auth/authRoutes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  controlRegister,
+  controlLogin,
+  controlLogout,
+} from "../controllers/authController";
+
+const router = express.Router();
+
+router.post("/register", controlRegister);
+
+router.post("/login", controlLogin);
+
+router.post("/logout", controlLogout);
+
+export default router;
